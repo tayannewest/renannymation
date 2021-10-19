debd2f3dce974bac9133231a4b851df954dbf9f6
Tay West <tayanne.west@gmail.com>
2021-10-19 14:24:43 -0400

    add pics and work on mood condition

--- js/app.js
+++ js/app.js
@@ -1,54 +1,5 @@
 
 
-
-
-
-
-// Renannymation: It’s Alive! (let’s keep it that way) pseudo code
-
-// Open on intro page
-// Fade in intro text (little blurb about game story)
-// Create ‘Interested?’ button to click to see instructions
-// Open on play screen with Adam, his status bar, and play icons
-// Guide player through icons (feed, teach, love) << (maybe add more options if I’m ahead)
-// 	create click events that generate alerts on feed, teach, and love to let player know 			how to use them
-// Create ‘I’ll do it’ button to click to initialize game
-// On ‘I’ll do it’ click, alert ‘Great, be back soon!’ and set timer for 3 min
-// Let player click icons to affect Adam
-// 	feed: 3 times every 30 seconds, more and he gets sick, less and he gets hungry
-// 	teach: 3 time every 30 seconds, more and he gets confused, less and he gets bored
-// 	love: unlimited times allowed, but < 3 every 30 seconds he feels neglected
-// Status bar starts half full
-// Feed: Start 30 second timer on game start
-// 		click feed icon to trigger feed event
-// 		if clicked 3 times, status bar increase by 5
-// 		if clicked < or > 3 times, status bar decrease by 5
-// 		if clicked more than 3 times before timer runs out alert ‘I’m full!’
-// 			if clicked > 5 times, he throws up, you lose 
-// 			if click < 1, he is hungry, you lose
-// 			fade to feed lose condition and ask if they want to try again
-// 				(replay button)
-// Teach: Start 30 second timer on game start
-// 		click teach icon to trigger teach event
-// 		if clicked 3 times, status bar increase by 5
-// 		if clicked < or > 3 times, status bar decrease by 5
-// 		if clicked more than 3 times before timer runs out, alert ‘Slow down, I can’t read that fast!’
-// 			if clicked > 5 times, he gets angry and tears up the book, you lose
-// 			if clicked < 1, he gets bored and tears up the book, you lose
-// 			fade to teach lose condition and ask if you want to try again
-// 				(replay button)
-// Love: Start 30 second timer on game start
-// 		click icon to trigger love event
-// 		if clicked >= 3 times, status bar increase by 5
-// 		if clicked < 3 times, status bar decrease by 5		
-// 		if clicked < 2 times every 30 seconds, alert  ‘I need a hug’ << (maybe add a list of pleas for affection to randomly sort through)
-// 			if clicked < 1 time, he runs away feeling unloved, congrats, you’ve unleashed a monster upon the town, fade to love lose condition and ask if you want to try again
-// 				(replay button)
-// 			If click is spammed and we hit more than 20 clicks in a minute, you can speed run the game and Adam feels validated and you win
-// If status bars have >= 50 points at end of 3 minutes
-// 	You win! Display win condition alert
-
-
 /*----------------- Constants -----------------*/
 
 
@@ -92,7 +43,7 @@ heart.addEventListener('click', rightChoice)
 
 function switchImg () {
   acceptBtn.hidden = true
-  document.querySelector('#game-img').src = 'https://picsum.photos/400/400'
+  document.querySelector('#game-img').src = 'images/sad.png'
   bgImg.className = 'animate__animated animate__fadeOut'
   bgImg.className = 'animate__animated animate__fadeIn'
   startBtn.removeAttribute('hidden')
@@ -150,11 +101,12 @@ function mood() {
     currentScore.innerHTML = 'unhappy...'
   } else if ((points >= 50) && (points <= 99)) {
     currentScore.innerHTML = 'feeling better...'
+    document.querySelector('#game-img').src = 'images/better.png'
   } else if (points >= 100) {
     currentScore.innerHTML = 'Happy!'
+    document.querySelector('#game-img').src = 'images/happy.png'
   }
 }
-console.log(mood())
 
 function render(){
   let timerId = setInterval(() => {
